ce5888f18bec43829c6062cecab95ac81664717b
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-10-09 15:49:54 +0100

    update UI colors

--- ui.py
+++ ui.py
@@ -12,18 +12,18 @@ class App(object):
         self.root.configure(bg="#769ea6")
         self.root.wm_title("CompCorrector")
 
-        self.label = Label(self.root, text="Choose a directory")
-        self.label.pack()
+        self.label = Label(self.root, text="Choose a directory \n(should contain subdirectories, which contain files to be graded)")
+        self.label.pack(pady=5)
 
         # set text entry
         self.input_dir = StringVar()
         self.entry_input_dir = Entry(self.root, textvariable=self.input_dir, width="400")
-        self.entry_input_dir.pack()
+        self.entry_input_dir.pack(pady=5)
 
         # set open dir button
         self.buttontext = StringVar()
         self.buttontext.set("Open")
-        Button(self.root, textvariable=self.buttontext, command=self.open_file).pack()
+        Button(self.root, textvariable=self.buttontext, command=self.open_file, bg="#ffff66").pack(pady=5)
 
         # check-boxes
         self.rm_zips = IntVar()
@@ -35,13 +35,13 @@ class App(object):
         self.rm_dirs = IntVar()
         self.rm_dirs.set(False)
         self.check_rm_dirs = Checkbutton(self.root, variable=self.rm_dirs, onvalue=True, offvalue=False,
-                                         text="Remove empty directories after moving files")
+                                         text="Remove empty directories")
         self.check_rm_dirs.pack()
 
         # set open dir button
         self.buttontext2 = StringVar()
         self.buttontext2.set("Start")
-        Button(self.root, textvariable=self.buttontext2, command=self.do_work).pack()
+        Button(self.root, textvariable=self.buttontext2, command=self.do_work, bg="#93b185").pack(pady=5)
 
         self.root.mainloop()
 
